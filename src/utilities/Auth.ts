0d17139b79d76c2d3e9c16280050b33af9ddfaa9
00acount <146901089+00acount@users.noreply.github.com>
2023-10-10 21:38:23 +0100

    Update Auth.ts

--- src/utilities/Auth.ts
+++ src/utilities/Auth.ts
@@ -1,5 +1,6 @@
 import { createContext, useContext } from 'react';
 import { Role } from './roles';
+import { API_URL } from './backend-api';
 
 export type AuthenticatedUser = {
     id: number
@@ -11,7 +12,7 @@ export const useAuth = () => useContext(authenticatedUserContext);
 
 
 export const getAuthenticatedUser = async () => {
-    const response = await fetch('http://localhost:8080/api/v1/whos-authenticated', {
+    const response = await fetch(`${API_URL}/api/v1/whos-authenticated`, {
             method: 'GET',
             credentials: "include",
         })
